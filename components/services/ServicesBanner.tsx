f53a551ee7eadafb5212d94a3489ca8a441fdf6f
BlackDev0222 <BlackDev0222@gmail.com>
2023-07-15 14:50:58 -0400

    implement services banner mobile responsive

--- components/services/ServicesBanner.tsx
+++ components/services/ServicesBanner.tsx
@@ -3,8 +3,8 @@
 import { PrimaryButton } from '../PrimaryButton'
 
 const ServicesBanner = () => {
-    return <div className='bg-services-banner bg-cover 2xl:ml-[-130px] xl:ml-[-130px] lg:ml-[-130px] h-[720px] 4xl:pl-[190px] 3xl:pl-[190px] 2xl:pl-[180px] xl:pl-[180px] lg:pl-[180px] pt-[114px]'>
-        <div className='w-[550px]'>
+    return <div className='bg-[#f5f5f5] bg-services-banner lg:bg-none md:bg-none sm:bg-none bg-cover h-[720px] lg:h-auto md:h-auto sm:h-auto 2xl:ml-[-130px] xl:ml-[-130px] 4xl:pl-[190px] 3xl:pl-[190px] 2xl:pl-[180px] xl:pl-[180px] px-[50px] pt-[114px] lg:py-[50px] md:py-[50px] sm:py-[50px]'>
+        <div className='w-[550px] lg:w-full md:w-full sm:w-full'>
             <span className='text-bannerTextSize text-bannerTextColor leading-[63.5px] font-[400] font-arial'>Senior care services designed to <span className='text-primary font-[700] font-arial'>support and assist</span> you in the comfort of your home.</span>
         </div>
         <div className='mt-[30px]'>
