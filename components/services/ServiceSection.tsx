9da0180771e37be293574e0fdd3d347b0af2fe30
BlackDev0222 <gabrieldeveloper1@gmail.com>
2023-07-16 00:41:44 -0400

    Update ServiceSection.tsx

--- components/services/ServiceSection.tsx
+++ components/services/ServiceSection.tsx
@@ -8,7 +8,7 @@ const ServiceSection = () => {
 		<p className='text-right text-bannerTextColor font-[300] text-[24px]'>Services</p>
 		<div className='flex mt-[42px] mb-[46px] h-[488px]'>
 			<Image className='' src={'/images/services_01.png'} alt="services_01" width={478} height={488}></Image>
-			<div className='ml-[320px] h-full'>
+			<div className='ml-[320px]'>
 				<p className='font-arial text-[45px] text-bannerTextColor font-[700]'>Personal Care Assistance</p>
 				<div>
 					<span className='font-arial text-[40px] text-bannerTextColor font-[300] h-[200px] leading-none'>Our caregivers provide assistance with
