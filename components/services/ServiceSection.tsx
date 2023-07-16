5ee6742fd33297f52eadeaf664aff8bb3c0c7580
BlackDev0222 <BlackDev0222@gmail.com>
2023-07-16 00:09:49 -0400

    create servicesection

--- components/services/ServiceSection.tsx
+++ components/services/ServiceSection.tsx
@@ -5,9 +5,9 @@ import { PrimaryButton } from '../PrimaryButton';
 
 const ServiceSection = () => {
 	return <section className='px-[227px] py-[41px]'>
-		<p className='text-right'>Services</p>
+		<p className='text-right text-bannerTextColor font-[300] text-[24px]'>Services</p>
 		<div className='flex mt-[42px] mb-[46px] h-[488px]'>
-			<Image className='' src={'/images/services_01.png'} alt="services_01" width={478} height={488} ></Image>
+			<Image className='' src={'/images/services_01.png'} alt="services_01" width={478} height={488}></Image>
 			<div className='ml-[320px] h-full'>
 				<p className='font-arial text-[45px] text-bannerTextColor font-[700]'>Personal Care Assistance</p>
 				<div>
