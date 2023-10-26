7b136851c562d266059a74d238d3f461ea77d036
techguru0 <130264564+techguru0@users.noreply.github.com>
2023-10-26 04:56:39 -0400

    Revert

---
+++ components/auth/Registration/GenderSelection.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+import React, { FC } from "react";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#CB5A6F",
+      dark: "#CB5A6F",
+      // light: will be calculated from palette.primary.main,
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+  },
+});
+
+const GenderSelection: FC = () => {
+  const [gender, setGender] = React.useState("20");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setGender(event.target.value);
+  };
+
+  return (
+    <div>
+      <div className="text-xs font-arial font-normal text-distlineColor">
+        {"Gender"}
+      </div>
+      <FormControl fullWidth={true} sx={{ my: 1, minWidth: 120 }}>
+        <ThemeProvider theme={theme}>
+          <Select
+            value={gender}
+            onChange={handleChange}
+            displayEmpty
+            inputProps={{ "aria-label": "Without label" }}
+            style={{ borderRadius: "6px" }}
+          >
+            <MenuItem value={10}>Male</MenuItem>
+            <MenuItem value={20}>Female</MenuItem>
+          </Select>
+          {/* <FormHelperText>Without label</FormHelperText> */}
+        </ThemeProvider>
+      </FormControl>
+    </div>
+  );
+};
+
+GenderSelection.displayName = "GenderSelection";
+
+export default GenderSelection;
